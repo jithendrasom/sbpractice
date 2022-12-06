e398dcaf6389ce39c3a11ddb6008aefece6585e1
jithendra <jithmailsu@gmail.com>
2022-12-06 14:24:48 +0530

    Initial commit

---
+++ sbpractice/src/main/java/com/sbpractice/SbpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.sbpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpracticeApplication.class, args);
+	}
+
+}
---
+++ sbpractice/src/test/java/com/sbpractice/SbpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
